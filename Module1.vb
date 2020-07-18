d4e4a4d196a4caa1367f84004951e601c4a5238d
nekosan23 <quintinguillaume@outlook.com>
2020-07-18 17:11:48 -0400

    Initial commit

---
+++ Module1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.Console
+
+Module Module1
+
+    'When use please clear after changing sub to prevent loop
+    Public FailSafeInteger As Integer = 0
+
+    Public wlc, cmdnf, cmdl, ld As String
+
+    Sub Main()
+        'Setting windows title (below should do this : just having fun V 0.1.0.0)
+        Title = "Just having fun V " + My.Application.Info.Version.ToString
+        'writing down the version number for the updater
+        'WritingVersionForUpdate()
+        'Loading configuration
+        ConfigurationLoader()
+    End Sub
+
+    'Writing down the version so the updater know the version and if a update is available and also launching the updater to update
+    Sub WritingVersionForUpdate()
+        'checking if the version file is existing if so delete to make a new one
+        If My.Computer.FileSystem.FileExists(My.Computer.FileSystem.CurrentDirectory + "/version.txt") Then
+            My.Computer.FileSystem.DeleteFile(My.Computer.FileSystem.CurrentDirectory + "/version.txt")
+            WritingVersionForUpdate()
+        Else
+            My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.CurrentDirectory + "/version.txt", My.Application.Info.Version.Major.ToString + My.Application.Info.Version.Minor.ToString + My.Application.Info.Version.Build.ToString + My.Application.Info.Version.Revision.ToString, True)
+        End If
+    End Sub
+
+    'Reading configuration file if it exist else it should make new configuration and ask the user
+    Sub ConfigurationLoader()
+        If My.Settings.UsersLanguage = "" AndAlso FailSafeInteger = 1 Then
+            WriteLine("Exception thrown FailSafe is Triggered unsaved settings exception")
+            ReadLine()
+        ElseIf My.Settings.UsersLanguage = "" Then
+            Static Dim UserInput As String
+            WriteLine("Hello Users it look's like it the first time we meet please enter the language i shall speak in below (for french type fr for english type en) :")
+            WriteLine("Bonjours Utilisateur il semble que c'est la première fois qu'on ce rencontre quel language tu veux que je parle en dessous de ce message (Pour Français écris fr et pour anglais en) :")
+            UserInput = ReadLine()
+
+            Select Case UserInput
+                Case "en"
+                    'User asked for english so saving that choice and reloading configurationloader but with a failsafe
+                    My.Settings.UsersLanguage = "en"
+                    My.Settings.Save()
+                    FailSafeInteger = 1
+                    Clear()
+                    ConfigurationLoader()
+                Case "fr"
+                    'User asked for french so saving that choice and reloading configurationloader but with a failsafe
+                    My.Settings.UsersLanguage = "fr"
+                    My.Settings.Save()
+                    FailSafeInteger = 1
+                    Clear()
+                    ConfigurationLoader()
+                Case Else
+                    Clear()
+                    WriteLine("Please retry this option doesn't exist")
+                    WriteLine("Merci de réessayez cette option n'existe pas")
+                    WriteLine("")
+                    WriteLine("Press enter to retry")
+                    WriteLine("Veuillez appuyez sur entrez pour réessayez")
+                    ReadLine()
+                    Clear()
+                    ConfigurationLoader()
+            End Select
+            'User has existing settings so not asking again
+        Else
+            Clear()
+            MainLanguageLoader(My.Settings.UsersLanguage)
+        End If
+    End Sub
+
+    'Loading all the text into the selected language into string variable **work in progress
+    Sub MainLanguageLoader(LanguageSelected As String)
+        Select Case LanguageSelected
+            Case "en"
+                ld = My.Resources.lden
+                WriteLine(ld)
+                wlc = My.Resources.wlcen
+                cmdnf = My.Resources.cmdnfen
+                cmdl = My.Resources.cmdlen
+            Case "fr"
+                ld = My.Resources.ldfr
+                WriteLine(ld)
+                wlc = My.Resources.wlcfr
+                cmdnf = My.Resources.cmdnffr
+                cmdl = My.Resources.cmdlfr
+        End Select
+        Clear()
+        ConsoleHome()
+    End Sub
+
+    'This is just the console homepage it also give all the command a point to return to
+    Sub ConsoleHome()
+        Static Dim Input
+        Clear()
+        WriteLine(wlc)
+        Input = ReadLine()
+        ReadingCommands(Input)
+    End Sub
+
+    'Reading the sended command
+    Sub ReadingCommands(Commands As String)
+        Select Case Commands
+            Case "gethardware"
+
+            Case "help"
+
+        End Select
+    End Sub
+    'GetHardwareInfo
+    Sub GetHardwareInfo()
+
+    End Sub
+    'Help Command
+    Sub Help()
+        WriteLine("this is the current list of available programs :")
+        WriteLine("gethardware -- start the GetHardwareInfo() allowing you to ask for specific info or just to browse what VB .net can acces")
+        WriteLine("more coming soon...")
+        WriteLine("press enter to continue..")
+        ReadLine()
+        ConsoleHome()
+    End Sub
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Just having fun")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Just having fun")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9f28b315-0c78-4000-b658-58b8a7eaad3e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.0.5")>
+<Assembly: AssemblyFileVersion("0.1.0.5")>
